a65bae95a0d9b369cc5e441570d701052759029c
hao.kou <kouhao2016@126.com>
2019-07-03 16:41:04 +0800

    init files

---
+++ src/main/java/com/example/client01/controller/TestController.java
@@ -0,0 +1,69 @@
+package com.example.client01.controller;
+
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
+import com.netflix.discovery.shared.Application;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@RestController
+@RequestMapping("/provider/order")
+public class TestController {
+
+    @Resource
+    private EurekaClient client;
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(TestController.class);
+
+    @GetMapping("/discovery")
+    public Object discovery() {
+        // 获取Eureka中所有的服务节点
+        List<Application> applications = client.getApplications().getRegisteredApplications();
+        if (applications != null) {
+            for (Application application : applications) {
+                // 对外暴露的服务名称
+                String name = application.getName();
+                // 只看订单服务信息
+//                if ("CLIENT-01".equals(name)) {
+                    // 服务有多少个实例，比如订单服务可能部署了多个，有多个订单服务注册到了eureka
+                    List<InstanceInfo> instances = application.getInstances();
+                    LOGGER.info("所有的订单服务：{}", instances);
+                    if (instances != null) {
+                        for (InstanceInfo info : instances) {
+                            LOGGER.info("服务id：{}", info.getInstanceId());
+                            LOGGER.info("服务主机：{}", info.getHostName());
+                            LOGGER.info("服务端口：{}", info.getPort());
+                        }
+                    }
+//                }
+            }
+        }
+        return null;
+    }
+
+    @Resource
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+
+    @Value("${spring.config.hello}")
+    private String hello;
+
+
+    @GetMapping("/hello/{message}")
+    public String from(@PathVariable String message) {
+
+        kafkaTemplate.send("test",message);
+
+
+        return this.hello;
+    }
+}
